f8a96031d68bf6d3a7d3dbd817cd5814eaf862d2
Papaya Labs <papayalabs@gmail.com>
2019-03-25 22:25:12 +0100

    Initial Commit

---
+++ cheap-advisor/app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def contact
+  end
+
+  def home
+  	puts 'Este es mi home'
+  end
+end
---
+++ cheap-advisor/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'about', to: 'pages#about'
+  get 'contact',to: 'pages#contact'
+  get '/', to: 'pages#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
